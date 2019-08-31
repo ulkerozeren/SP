72faef25803d75d80e018f4f4a740594fbf886a4
ulkerozeren <53277896+ulkerozeren@users.noreply.github.com>
2019-08-31 22:20:05 +0300

    Update sp.sql

--- sp.sql
+++ sp.sql
@@ -23,3 +23,11 @@ select @res as [sum], @x as x, @y as y
 -----------------------
 
 exec B 34,5676
+-------
+create procedure topla(@x int, @y int, @sonuc int output)
+as
+set @sonuc=@x+@y
+
+declare @t int
+exec topla 4,56,@t output
+print 'toplam: ' + cast(@t as nvarchar(50))
